5f04e0641d4a89a8107a3d7de089354b63996376
Renato Leandro <renatoleandrosouza@hotmail.com>
2022-10-17 15:06:46 -0300

    Adicionar arquivos de projeto.

---
+++ EscolarMysql/Data/Cadastro/CadastroDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EscolarMysql.Data.Cadastro
+{
+    public class CadastroDbContext : DbContext
+    {
+        public string strConnection;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //-->Conexao com servidor local
+            const string strConnection = "Server=oficiotec.ddns.net;User Id=root;Password=@cpdrede12;Database=Oficio10Dev";
+            optionsBuilder
+            .UseMySql(strConnection, ServerVersion.AutoDetect(strConnection));
+
+        }
+
+
+        //public DbSet<Programa> programa { get; set; }
+        //public DbSet<sistema> sistema { get; set; }
+        //public DbSet<tipomenu> tipomenu { get; set; }
+    }
+}
---
+++ EscolarMysql/Data/Cadastro/CadastroDbContextBase.cs
@@ -0,0 +1,15 @@
+﻿namespace EscolarMysql.Data.Cadastro
+{
+    public class CadastroDbContextBase
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //-->Conexao com servidor local
+            const string strConnection = "Server=oficiotec.ddns.net;User Id=root;Password=@cpdrede12;Database=Oficio10Dev";
+            optionsBuilder
+            .UseMySql(strConnection, ServerVersion.AutoDetect(strConnection));
+
+        }
+    }
+}
